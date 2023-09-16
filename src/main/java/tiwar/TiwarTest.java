ccb80e3146322c2607fc176eb39e60bc16aeb463
Sourav Datta <souravdatta1995@gmail.com>
2023-09-16 19:42:04 +0530

    Change to firefox

--- src/main/java/tiwar/TiwarTest.java
+++ src/main/java/tiwar/TiwarTest.java
@@ -8,7 +8,6 @@ import java.util.concurrent.TimeUnit;
 public class TiwarTest {
     public static void main(String[] args) {
         Configuration.baseUrl = "http://tiwar.net";
-        Configuration.headless = true;
         Timer timer = new Timer();
         timer.schedule(new TiwarTasks(), 0, TimeUnit.MINUTES.toMillis(3));
     }
