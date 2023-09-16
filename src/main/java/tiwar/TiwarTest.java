074821e364b4cfe74d2b28501bcd5cf7ac8a5754
Sourav Datta <souravdatta1995@gmail.com>
2023-09-16 17:43:35 +0530

    Change to firefox

--- src/main/java/tiwar/TiwarTest.java
+++ src/main/java/tiwar/TiwarTest.java
@@ -3,6 +3,8 @@ package tiwar;
 import com.codeborne.selenide.Browsers;
 import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 
 import java.util.Timer;
 import java.util.concurrent.TimeUnit;
@@ -10,7 +12,9 @@ import java.util.concurrent.TimeUnit;
 public class TiwarTest {
     public static void main(String[] args) {
         Configuration.browser = Browsers.EDGE;
-        Configuration.browserCapabilities = new EdgeOptions();
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--headless=new");
+        Configuration.browserCapabilities = firefoxOptions;
         Configuration.baseUrl = "http://tiwar.net";
         Timer timer = new Timer();
         timer.schedule(new TiwarTasks(), 0, TimeUnit.MINUTES.toMillis(3));
