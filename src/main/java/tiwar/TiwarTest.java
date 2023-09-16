d67fab7c4310ec0724eaa3f429f73d6247901017
Sourav Datta <souravdatta1995@gmail.com>
2023-09-16 19:51:37 +0530

    Change to firefox

--- src/main/java/tiwar/TiwarTest.java
+++ src/main/java/tiwar/TiwarTest.java
@@ -11,6 +11,12 @@ public class TiwarTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(TiwarTest.class);
 
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide the WebDriver path as an argument.");
+            return;
+        }
+        String webDriverPath = args[0];
+        System.setProperty("webdriver.gecko.driver", webDriverPath);
         Configuration.baseUrl = "http://tiwar.net";
         LOGGER.info("Tiwar Automate STarted with "+Configuration.browser+" Headless "+Configuration.headless);
         Timer timer = new Timer();
