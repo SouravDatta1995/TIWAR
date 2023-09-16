0066265c21f8539c7554361c191927a49f276b5d
Sourav Datta <souravdatta1995@gmail.com>
2023-09-16 19:11:01 +0530

    Change to firefox

--- src/main/java/tiwar/TiwarTest.java
+++ src/main/java/tiwar/TiwarTest.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 
 public class TiwarTest {
     public static void main(String[] args) {
-        Configuration.browser = Browsers.EDGE;
+        Configuration.browser = Browsers.FIREFOX;
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.addArguments("--headless=new");
         Configuration.browserCapabilities = firefoxOptions;
